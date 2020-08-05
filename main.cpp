254733a134a98186868ea0c1969c637e1829c080
Yuval Rotman <Yrotman2604@gmail.com>
2020-08-05 17:16:37 +0300

    Checking implementation at main.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include "cats_inheritance/cat.h"
 #include "cats_inheritance/siami_street_cat.h"
+#include "cats_inheritance/street_cat.h"
+#include "cats_inheritance/siami_cat.h"
+
+
+using std::cout, std::endl;
 
 int main() {
-    SiamiStreetCat *c = new SiamiStreetCat("Mitzi", "Brown", 12.37, 7
-                                           , "Pizza");
 
-    c->show();
+    SiamiStreetCat ssc("Mitzi", "Brown", 2.35, 4, "Pizza");
+    StreetCat *sc = new StreetCat((StreetCat&) ssc);
+    sc->show();
 
-    delete c;
+    delete sc;
+
     return 0;
 }
